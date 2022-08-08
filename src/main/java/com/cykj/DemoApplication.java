e7d58724bb6382a2abf5c0a6e8a9d5d2704c948a
Garry <444243459@qq.com>
2022-08-08 12:27:35 +0800

    test test123

--- src/main/java/com/cykj/DemoApplication.java
+++ src/main/java/com/cykj/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
         System.out.println("不吃饭");
         System.out.println("吃饭");
         System.out.println("帆帆是🐖");
-        System.out.println("test test");
+        System.out.println("test test123");
     }
 
 }
